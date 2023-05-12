c53c6e2edfb38a1859fea5beb176fe1a848f3e16
Anna Gerasymenko <angor8@gmail.com>
2023-05-12 12:12:15 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
-from blog.models import Tag, Post
-
-
-# Register your models here.
-admin.site.register(Tag)
-
+from blog.models import Tag, Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ["title", "published_at", "slug", "title" ]
     
 
+# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Comment)
 admin.site.register(Post, PostAdmin)
+
 
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 # Create your models here.
@@ -8,6 +11,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.value
+ 
+class Comment(models.Model):
+# Generic Relationships
+  # A ForeignKey field that points to a ContentType. Normally this is called content_type
+  # A PositiveIntegerField that stores the primary key of the related object. Normally this is called object_id
+  # A GenericForeignKey field, a special type of field that will look up the object from the other two new fields.
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
@@ -19,6 +36,7 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title
